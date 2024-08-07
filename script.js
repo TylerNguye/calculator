daad3bba94b6bf48217d4015b5e36af88b1e3bf5
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-07 18:12:54 -0400

    Add four basic math operations

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(operand2, operator, operand1) {
+    switch(operator) {
+        case '+':
+            return add(operand1, operand2)
+        case '-':
+            return subtract(operand1, operand2)
+        case '*':
+            return multiply(operand1, operand2)
+        case '/':
+            return divide(operand1, operand2)
+    }
+}
